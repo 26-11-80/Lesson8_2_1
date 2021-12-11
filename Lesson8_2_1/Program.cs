69dbe6b4c81e81957d89d14da58a86fad7b84a94
anvarovagulnaz <anvarovagulnaz@gmail.com>
2021-12-11 13:16:13 +0500

    Добавьте файлы проекта.

---
+++ Lesson8_2_1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson8_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/For lesson 8/text_files/text8_2.txt";
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("десять рандомных чисел -");
+                {
+                    int[] array = new int[10];
+                    Random random = new Random();
+                    int S = 0;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        array[i] = random.Next(0, 10);
+                        S += array[i];
+                        sw.Write("{0}", array[i]);
+                        sw.WriteLine();
+                    }
+                    //sw.WriteLine();
+                    //sw.WriteLine("Сумма чисел - ");
+                    //sw.WriteLine(S);                  - это небыло условием, но мне понравилось)
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        Console.WriteLine(sr.ReadToEnd());
+                    }
+                    //Console.WriteLine();
+                    // Console.WriteLine("Сумма чисел из файла -");
+                    //Console.WriteLine(S);                         - должно работать
+                    Console.ReadKey();
+                }
+                                 
+            }
+        }
+    }
+}
+
+    
+                
+            
+
+
+
+        
+    
+
---
+++ Lesson8_2_1/Program_8_2_2_end.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson8_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = "Logs2/SumRandom.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            // DirectoryInfo directory = new DirectoryInfo(path);
+            // if(!directory.Exists)
+            {
+            //    directory.Create();
+                
+            }
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+               sw.WriteLine("десять рандомных чисел -");
+                {
+                  int[] array = new int[10];
+                  Random random = new Random();
+                  int S = 0;
+                  for (int i = 0; i < 10; i++)
+                    {
+                     array[i] = random.Next(0, 10);
+                     S += array[i];
+                     sw.Write("{0}", array[i]);
+                    }
+
+                  sw.WriteLine();
+                  sw.WriteLine("Сумма чисел - ");
+                  sw.WriteLine(S);      //- это не было условием, но как вариант записи в файл )
+                    {
+                        
+                            using (StreamReader sr = new StreamReader(path))       // - тут проблема - пишет, что занят другим процессом, пробовала по разному, менять директории, но нет)
+                            {
+                                Console.WriteLine(sr.ReadLine());
+
+                            }
+                        // соответственно, остальное работает, сумму из файла можно считать из формулы выше, но из за проблемы считывания нет смысла подставлять ниже
+                     Console.WriteLine();
+                     Console.WriteLine("Сумма чисел из файла -");
+                     Console.WriteLine(S);                        
+                     Console.ReadKey();
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
+    
+                
+            
+
+
+
+        
+    
+
